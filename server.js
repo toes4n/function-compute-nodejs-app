8536e12ec7ba36883a75e4c906546d5a07bbd6eb
TS <ts@ts>
2026-02-06 14:44:54 +0700

    init

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ function getRandomColor() {
   return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
 
-server.listen(3000, () => {
-  console.log('Game server running on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Game server running on port ${PORT}`);
 });
